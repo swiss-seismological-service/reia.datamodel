2555e77fe32cd0138fae56676aa40e86f9c46fba
Nicolas Schmid <nicolas@breiten.ch>
2022-10-21 15:43:14 +0200

    chores: add m2m table to exports

--- esloss/datamodel/__init__.py
+++ esloss/datamodel/__init__.py
@@ -4,9 +4,9 @@ from esloss.datamodel.calculations import (DamageCalculation, EStatus,  # noqa
                                            LossCalculation, RiskCalculation)
 from esloss.datamodel.earthquake import EarthquakeInformation  # noqa
 from esloss.datamodel.earthquake import EEarthquakeType  # noqa
-from esloss.datamodel.lossvalues import AggregatedLoss  # noqa
-from esloss.datamodel.lossvalues import (ELossCategory, LossValue,  # noqa
-                                         SiteLoss)
+from esloss.datamodel.lossvalues import (AggregatedLoss, ELossCategory,  # noqa
+                                         LossValue, SiteLoss,
+                                         aggregatedloss_aggregationtag)
 from esloss.datamodel.vulnerability import (  # noqa
     BusinessInterruptionVulnerabilityModel, ContentsVulnerabilityModel,
     LossRatio, NonstructuralVulnerabilityModel, OccupantsVulnerabilityModel,
