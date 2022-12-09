628a7d7ede1404e1b9fa821974ec1634165a6c21
Nicolas Schmid <nicolas@breiten.ch>
2022-12-09 14:29:17 +0100

    feat: update pk's and fk's to work with partitions

--- reia/datamodel/lossvalues.py
+++ reia/datamodel/lossvalues.py
@@ -1,6 +1,6 @@
 import enum
 
-from sqlalchemy import Table
+from sqlalchemy import ForeignKeyConstraint, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column, ForeignKey
 from sqlalchemy.sql.sqltypes import BigInteger, Enum, Float, Integer, String
@@ -20,33 +20,44 @@ class ELossCategory(enum.Enum):
 riskvalue_aggregationtag = Table(
     'loss_assoc_riskvalue_aggregationtag',
     ORMBase.metadata,
-    Column('riskvalue', ForeignKey(
-        'loss_riskvalue._oid',
-        ondelete='CASCADE'),
-        primary_key=True),
+    Column('_oid', BigInteger),
+    Column('riskvalue', BigInteger),
     Column('aggregationtag', ForeignKey(
         'loss_aggregationtag._oid',
-        ondelete='CASCADE'),
-        primary_key=True),
-    Column('losscategory', ELossCategory),
+        ondelete='CASCADE')),
+    Column('losscategory', Enum(ELossCategory)),
     Column('_calculation_oid', BigInteger),
+
+    ForeignKeyConstraint(['riskvalue', 'losscategory', '_calculation_oid'],
+                         ['loss_riskvalue._oid', 'loss_riskvalue.losscategory',
+                          'loss_riskvalue._calculation_oid'],
+                         ondelete='CASCADE'),
+
     postgresql_partition_by='LIST (_calculation_oid)'
 )
 
 
 class RiskValue(ORMBase):
     __table_args__ = {
-        'postgresql_partition_by': 'LIST (_calculation_oid)'
+        'postgresql_partition_by': 'LIST (_calculation_oid)',
     }
+
+    __mapper_args__ = {
+        'primary_key': ['loss_riskvalue.c._oid',
+                        'loss_riskvalue.c._calculation_oid',
+                        'loss_riskvalue.c.losscategory']
+    }
+
+    _oid = Column(BigInteger, primary_key=True)
     # id of the realization
     eventid = Column(Integer, nullable=False)
-    losscategory = Column(Enum(ELossCategory), nullable=False)
+    losscategory = Column(Enum(ELossCategory), nullable=False, primary_key=True)
     weight = Column(Float)
 
     _calculation_oid = Column(BigInteger,
                               ForeignKey('loss_calculation._oid',
                                          ondelete='CASCADE'),
-                              nullable=False)
+                              nullable=False, primary_key=True)
     aggregationtags = relationship('AggregationTag',
                                    secondary=riskvalue_aggregationtag,
                                    back_populates='riskvalues',
